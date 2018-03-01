6fecdfed95d397ff22eb90bff0fe0cb0ca50ff49
Unknown <mdovturner@gmail.com>
2018-03-01 13:16:29 -0600

    lunch break
    move class user and class post to new models python file, add login code and logout function

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-
+from flask_login import LoginManager
 from flask import Flask
 from config import Config
 
@@ -11,23 +11,6 @@ from flask_migrate import Migrate
 db = SQLAlchemy(app2)
 migrate = Migrate(app2, db)
 
-
-class User(db.Model):
-   id = db.Column(db.Integer, primary_key=True)
-   username = db.Column(db.String(64), index=True, unique=True)
-   email = db.Column(db.String(120), index=True, unique=True)
-   password_hash = db.Column(db.String(128))
-
-   def __repr__(self):
-       return '<User {}>'.format(self.username)
-
-class Post(db.Model):
-   id = db.Column(db.Integer, primary_key=True)
-   title = db.Column(db.String(20))
-   subtitle = db.Column(db.String(40))
-   body = db.Column(db.String(280))
-   user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-   def __repr__(self):
-       return '<Post {}>'.format(self.body)
+login = LoginManager(app2)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from app import app2 as app
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app.Forms import LoginForm
-from app import Post
+from app.models import Post, User
+from flask_login import current_user, login_user, logout_user
 
 @app.route('/')
 @app.route('/Index')
@@ -33,10 +34,26 @@ def store():
         ]
         return render_template('store.html', items5=items6)
 
-@app.route('/login', methods=["GET", "POST"])
+@app.route('/Login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('Index'))
     form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user{}, remember_me={}'.format(form.username.data, form.remember_me.data))
-        return redirect('/login')
-    return render_template('login.html', title='Sign In', form=form)
+    if form.validate_on_submit():# post and submit validate
+
+        # get the user from data base use code
+        user = User.query.filter_by(username=form.username.data).first()
+
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('Login'))
+
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('Index'))
+
+    return render_template('Login.html', title='Sign In', form=form) # GET or submit validate Flaid
+
+@app.route('/logout')
+def logout():
+        logout_user()
+        return redirect(url_for('Index'))
